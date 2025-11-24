349603cd9dd6b8146eb449f81af23fead72a7b11
Francisco Manuel Toledo Pérez <ftolper633@cpifpalanturing.es>
2025-11-24 19:34:26 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- EjercicioClase/main.tf
+++ EjercicioClase/main.tf
@@ -24,63 +24,133 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_ingress" {
   ip_protocol = "TCP"
   cidr_ipv4 = "0.0.0.0/0"
 }
-//////////////////Security Group HTTP////////////////////
-resource "aws_security_group" "http" {
-  description = "Allow HTTP trafics"
-  name = "http-demo"
+////////////////////// SECURITY GROUP FRONTEND //////////////////////
+
+resource "aws_security_group" "frontend_sg" {
+  name        = "frontend-sg"
+  description = "Security Group for FrontEnd"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "http_ingress" {
-  security_group_id = aws_security_group.http.id
-  to_port = 80
-  from_port = 80
-  ip_protocol = "TCP"
-  cidr_ipv4 = "0.0.0.0/0"
+# SSH desde cualquier lado
+resource "aws_vpc_security_group_ingress_rule" "frontend_ssh" {
+  security_group_id = aws_security_group.frontend_sg.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
-//////////////////Security Group ALL EGRESS////////////////////
-resource "aws_security_group" "all" {
-  description = "Allow All Egress trafics"
-  name = "all-demo"
+# HTTP desde cualquier lado
+resource "aws_vpc_security_group_ingress_rule" "frontend_http" {
+  security_group_id = aws_security_group.frontend_sg.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_egress_rule" "all_egress" {
-  security_group_id = aws_security_group.all.id
-  ip_protocol = "-1"
-  cidr_ipv4 = "0.0.0.0/0"
+# Egress libre
+resource "aws_vpc_security_group_egress_rule" "frontend_egress" {
+  security_group_id = aws_security_group.frontend_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
-//////////////////Instancia Backend////////////////////
-resource "aws_instance" "Backend" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  key_name = var.key_name
-  vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.http.id,
-    aws_security_group.all.id,
-  ]
-    user_data = file("back.sh")
-    user_data_replace_on_change = true
-    tags = {
-      Name = "Backend_Server_Rute53"
-    }
+
+////////////////////// SECURITY GROUP BACKEND //////////////////////
+
+resource "aws_security_group" "backend_sg" {
+  name        = "backend-sg"
+  description = "Security Group for BackEnd"
 }
 
-//////////////////Instancia Frontend////////////////////
+# SSH desde cualquier lado
+resource "aws_vpc_security_group_ingress_rule" "backend_ssh" {
+  security_group_id = aws_security_group.backend_sg.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+# HTTP SOLO desde FrontEnd
+resource "aws_vpc_security_group_ingress_rule" "backend_http_from_frontend" {
+  security_group_id            = aws_security_group.backend_sg.id
+  referenced_security_group_id = aws_security_group.frontend_sg.id
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "tcp"
+}
+
+# Egress libre
+resource "aws_vpc_security_group_egress_rule" "backend_egress" {
+  security_group_id = aws_security_group.backend_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
+////////////////////// INSTANCIA FRONTEND //////////////////////
 
 resource "aws_instance" "Frontend" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  key_name = var.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+
   vpc_security_group_ids = [
-    aws_security_group.ssh.id,
-    aws_security_group.http.id,
-    aws_security_group.all.id,
+    aws_security_group.frontend_sg.id
   ]
-    user_data = file("front.sh")
-    user_data_replace_on_change = true
-    tags = {
-      Name = "Frontend_Server_Rute53"
-    }
+
+  user_data                  = file("front.sh")
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "Frontend_Server_Route53"
+  }
+}
+
+////////////////////// INSTANCIA BACKEND //////////////////////
+
+resource "aws_instance" "Backend" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.backend_sg.id
+  ]
+
+  user_data                  = file("back.sh")
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "Backend_Server_Route53"
+  }
+}
+
+
+//////////////////Route53////////////////////
+resource "aws_route53_zone" "default" {
+  name = var.domain
+  vpc {
+    vpc_id = data.aws_vpc.default_vpc.id
+  }
+}
+
+# Registro DNS para el FrontEnd
+resource "aws_route53_record" "frontend" {
+  zone_id = aws_route53_zone.default.id
+  name    = "frontend.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.Frontend.private_ip]
+}
+
+# Registro DNS para el BackEnd
+resource "aws_route53_record" "backend" {
+  zone_id = aws_route53_zone.default.id
+  name    = "backend.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.Backend.private_ip]
 }
